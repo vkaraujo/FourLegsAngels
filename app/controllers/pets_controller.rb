312f7a143f821933aae48c006d79d62231e2431f
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-02 11:50:34 -0300

    pet CRUD, private methods, create, destroy implemented

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,30 +1,39 @@
 class PetsController < ApplicationController
-  
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
   def index
     @pets = Pet.all
   end
 
   def show
-    @pet = Pet.find(params[:id]
   end
 
   def new
-    @ = .new
+    @pet = Pet.new
   end
   
+  def edit
+  end  
+
   def create
-    @object = Object.new(params[:object])
-    if @object.save
+    @pet = Pet.new(pet_params)
+    if @pet.save
       flash[:success] = "Object successfully created"
-      redirect_to @object
+      redirect_to @pet
     else
       flash[:error] = "Something went wrong"
       render 'new'
     end
   end
+
   
   private
 
-  def 
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end  
 
+  def pet_params
+    params.require().permit(:name, :specie, :size, :description)
+  end  
 end
