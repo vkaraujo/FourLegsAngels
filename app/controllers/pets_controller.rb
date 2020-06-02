b03ff60cda4f367b575e005519f8b926357eeb21
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-02 11:33:34 -0300

    pets CRUD

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,30 @@
 class PetsController < ApplicationController
+  
   def index
+    @pets = Pet.all
   end
 
   def show
+    @pet = Pet.find(params[:id]
   end
+
+  def new
+    @ = .new
+  end
+  
+  def create
+    @object = Object.new(params[:object])
+    if @object.save
+      flash[:success] = "Object successfully created"
+      redirect_to @object
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
+  private
+
+  def 
+
 end
