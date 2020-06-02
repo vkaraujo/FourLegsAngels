ccd8dd2dbd4d3d510007d875a7fe75e18273215f
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-06-02 14:21:43 -0300

    implemented requests controller and view

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,32 @@
+class RequestsController < ApplicationController
+  def index
+    @requests = Request.all
+  end
+
+  def new
+    @request = request.new
+  end
+
+  def create
+    @pet = Pet.find(params[:pet_id])
+    @request = Request.new(request_params)
+    @request.pet = @pet
+    if @request.save
+      redirect_to pet_path(@pet)
+    else
+      render 'pets/show'
+    end
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    @request.update(request_params)
+    redirect_to requests_path
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:status, :pet_id, :user_id)
+  end
+end
