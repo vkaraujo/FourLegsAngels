75baf268aa752bdc51809b4f6914d0ea6854ab6d
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-02 12:40:14 -0300

    add update and destroy at pets_controller and add pet index view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,6 +25,21 @@ class PetsController < ApplicationController
       render 'new'
     end
   end
+
+  # PATCH/PUT /pets/1
+  def update
+    if @pet.update(pet_params)
+      redirect_to @pet, notice: 'pet was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /pets/1
+   def destroy
+     @pet.destroy
+     redirect_to pets_url, notice: 'pet was successfully destroyed.'
+   end
   
   private
 
